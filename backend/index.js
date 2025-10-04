f0842e6fc46392b36031ba54a6c5045d1f6bcd0a
Rohan100 <rohannagare8355@gmail.com>
2025-10-04 19:12:13 +0530

    mongourl test

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ app.use(cors("*"));
 app.use(express.json());
 
 // connect to db
+console.log(process.env.MONGO_URL)
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Connected to Database"))
